ca66e6c81598af94131244628d9c660f93a070ae
João Pedro <joaopedrom4a1@gmail.com>
2024-10-15 11:43:13 -0300

    Get task by id implemented

--- src/main/Program.java
+++ src/main/Program.java
@@ -3,11 +3,15 @@ package main;
 import java.sql.Connection;
 
 import db.Database;
+import model.dao.DaoFactory;
+import model.entities.Task;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Connection conn = Database.getConnection();
+		Task t = DaoFactory.getTaskDao(conn).getById(2);
+		System.out.println(t);
 		Database.closeConnection();
 	}
 
--- src/model/dao/impl/TaskDaoJdbc.java
+++ src/model/dao/impl/TaskDaoJdbc.java
@@ -2,6 +2,8 @@ package model.dao.impl;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import model.dao.TaskDao;
@@ -9,16 +11,30 @@ import model.entities.Task;
 
 public class TaskDaoJdbc implements TaskDao {
 	
-	private Connection connection;
+	private Connection conn;
 
-	public TaskDaoJdbc(Connection connection) {
-		this.connection = connection;
+	public TaskDaoJdbc(Connection conn) {
+		this.conn = conn;
 	}
 
 	@Override
 	public Task getById(Integer id) {
-//		PreparedStatement st = null;
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		Task task = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT * FROM tasks WHERE id=?");
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			task = instantiateTask(rs);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return task;
 	}
 
 	@Override
@@ -43,6 +59,20 @@ public class TaskDaoJdbc implements TaskDao {
 	public void delete(Task t) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private static Task instantiateTask(ResultSet rs) throws SQLException {
+		Task t = null;
+		
+		if (rs.next()) {
+			t = new Task();
+			t.setId(rs.getInt("id"));
+			t.setMoment(rs.getDate("moment"));
+			t.setTitle(rs.getString("title"));
+			t.setDone(rs.getBoolean("done"));
+		}
+		
+		return t;
 	}
 
 }
